b4529da8cd2996e2d7e969d5cc192d327615a2ab
lihq <hello.lihq@outlook.com>
2020-07-01 00:12:50 +0800

    重写代码

--- sohu.py
+++ sohu.py
@@ -1,2 +1,14 @@
 from bs4 import BeautifulSoup
+import requests
 
+response = requests.get("https://www.sohu.com")
+bs = BeautifulSoup(response.text,"html.parser")
+
+result = bs.find_all("a")
+
+for link in result:
+    title = link.get("title")
+    if title is not None:
+        file = open("value1.txt","a+")
+        file.write(title)
+        file.close()
